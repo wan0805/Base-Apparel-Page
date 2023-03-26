98f2c95070398d4ab70546e2f704596d22f0df80
Wanderson duarte <92058106+wan0805@users.noreply.github.com>
2023-03-25 21:07:56 -0300

    Alterado os nomes das variáveis

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,14 +1,14 @@
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
 const button = document.querySelector("#button");
-const mensagem = document.querySelector("#message");
+const message = document.querySelector("#message");
 const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 
 function messageSucess(text){
-	mensagem.textContent = text;
-	mensagem.classList.remove("error");
-	mensagem.classList.add("sucess");
+	message.textContent = text;
+	message.classList.remove("error");
+	message.classList.add("sucess");
 	input.classList.remove("input--error");
 	input.value = "";
 
@@ -16,9 +16,9 @@ function messageSucess(text){
 }
 
 function messageError(text){
-	mensagem.textContent = text;
-	mensagem.classList.remove("sucess");
-	mensagem.classList.add("error");
+	message.textContent = text;
+	message.classList.remove("sucess");
+	message.classList.add("error");
 	input.classList.add("input--error")
 
 }
@@ -28,7 +28,7 @@ form.addEventListener("submit" , (e)=>{
 	e.preventDefault();
 })
 
-function verificarEmail(){
+function doCheckEmail(){
 	if (regexEmail.test(input.value)){
 		messageSucess("Email successfully sent!!!");
 	}
@@ -41,4 +41,4 @@ function verificarEmail(){
 }
 
 
-button.addEventListener("click", verificarEmail);
+button.addEventListener("click", doCheckEmail);
